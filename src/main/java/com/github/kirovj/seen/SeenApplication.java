11b0c05b43b4eca18361f702c42fb3429486914f
J-wyt <546108612@qq.com>
2021-10-09 01:33:16 +0800

    init proj

---
+++ src/main/java/com/github/kirovj/seen/SeenApplication.java
@@ -0,0 +1,13 @@
+package com.github.kirovj.seen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeenApplication.class, args);
+    }
+
+}

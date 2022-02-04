ed99bf5931fc66019cbd6a3c21f362f6f69b32a1
j-wyt <546108612@qq.com>
2022-02-04 21:02:17 +0800

    feat: add JSONUtils

---
+++ src/main/java/com/github/kirovj/seen/component/json/JSONUtils.java
@@ -0,0 +1,33 @@
+package com.github.kirovj.seen.component.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+/**
+ * @author : kirovj
+ * @date : 2022/2/4 20:57
+ */
+public class JSONUtils {
+
+    private JSONUtils() {}
+
+    private static final ObjectReader reader;
+
+    private static final ObjectWriter writer;
+
+    static {
+        var mapper = new ObjectMapper();
+        reader = mapper.reader();
+        writer = mapper.writer();
+    }
+
+    public static <T> String toStr(T t) throws JsonProcessingException {
+        return writer.writeValueAsString(t);
+    }
+
+    public static <T> T toObj(String s, Class<T> clazz) throws JsonProcessingException {
+        return reader.forType(clazz).readValue(s);
+    }
+}

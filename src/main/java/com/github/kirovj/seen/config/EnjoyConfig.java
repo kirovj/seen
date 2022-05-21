1c7c40143bea05da7adea7b180589f62ac0c88ee
J-wyt <546108612@qq.com>
2022-05-22 05:38:46 +0800

    feat: add html template engine - Enjoy

---
+++ src/main/java/com/github/kirovj/seen/config/EnjoyConfig.java
@@ -0,0 +1,23 @@
+package com.github.kirovj.seen.config;
+
+import com.jfinal.template.ext.spring.JFinalViewResolver;
+import com.jfinal.template.source.ClassPathSourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+
+@Configuration
+public class EnjoyConfig {
+
+    @Bean
+    public ViewResolver enjoyViewResolver() {
+        var jvr = new JFinalViewResolver();
+        jvr.setDevMode(false);
+        jvr.setSourceFactory(new ClassPathSourceFactory());
+        jvr.setPrefix("/templates/");
+        jvr.setSuffix(".html");
+        jvr.setContentType("text/html;charset=UTF-8");
+        jvr.setOrder(0);
+        return jvr;
+    }
+}

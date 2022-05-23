093bacf6a484165c94d841e90b1e603220eb6272
J-wyt <546108612@qq.com>
2022-05-23 23:53:33 +0800

    feat: add temp index.html

--- src/main/java/com/github/kirovj/seen/controller/IndexController.java
+++ src/main/java/com/github/kirovj/seen/controller/IndexController.java
@@ -1,15 +1,24 @@
 package com.github.kirovj.seen.controller;
 
+import com.github.kirovj.seen.domain.modal.Video;
+import com.github.kirovj.seen.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private VideoService videoService;
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("name", "enjoy");
+        List<Video> videoList = videoService.findAll();
+        model.addAttribute("videoList", videoList);
         return "index";
     }
 }
